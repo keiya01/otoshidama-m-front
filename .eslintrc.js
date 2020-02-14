89058fdab657a2790ebd52e8ac72e00a32301f86
keiya01 <keiya.s.0210@gmail.com>
2020-02-14 23:58:08 +0900

    Add  a11y plugin, hooks plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	parser: "@typescript-eslint/parser",
   extends: [ "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:prettier/recommended" ],
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y"],
 	parserOptions: {
 		ecmaVersion: 2019,
 		sourceType: "module",
@@ -14,7 +14,10 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-var-requires": 0,
 		"react/prop-types": 0,
-		"no-console": 2
+		"no-console": 2,
+		"react-hooks/rules-of-hooks": 2,
+		"react-hooks/exhaustive-deps": 1,
+		"jsx-a11y/rule-name": 1
   },
   settings:  {
     react:  {
