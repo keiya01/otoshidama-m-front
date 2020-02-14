722b40a37ce15138c4ac87b091157f7ba81a22bd
keiya01 <keiya.s.0210@gmail.com>
2020-02-15 01:24:00 +0900

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [ "airbnb-typescript" ],
   parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: "module",
-    ecmaFeatures: [ "jsx" ]
-  },
-  parserOptions: {
     project: './tsconfig.json',
   },
   rules: {
     "@typescript-eslint/no-dupe-class-members": 0
-  },
-  settings:  {
-    react:  {
-      version:  'detect'
-    }
   }
 }
