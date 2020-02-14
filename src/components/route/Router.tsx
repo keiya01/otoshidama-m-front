6769536334d9c89da29ea6d0c54dee378b88f760
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-02-15 00:52:26 +0900

    Introduce react-router

---
+++ src/components/route/Router.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { BrowserRouter, Switch } from "react-router-dom"
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
