704aad3d2761eaa4f8e87dba8bec9a1dee985500
keiya01 <keiya.s.0210@gmail.com>
2020-02-15 21:19:29 +0900

    Setup docz

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border-radius: 5px;
+  background-color: blue;
+  color: #fff;
+  font-size: 1rem;
+  padding: 5px 15px;
+`;
+
+export default Button;
