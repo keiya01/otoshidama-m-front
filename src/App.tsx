f5af30f68448be22f0e68ee200283f3962003732
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-02-15 23:22:31 +0900

    Introduce redux-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Router from './components/route/Router';
 import reducer from './reducer/reducer';
+import { composeReduxDevToolsEnhancers } from './store/store';
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, composeReduxDevToolsEnhancers());
 
 const App = () => (
   <div className="App">
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,3 +1,12 @@
+import { compose } from 'redux';
+
+interface ExtendedWindow extends Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+}
+declare let window: ExtendedWindow;
+
+export const composeReduxDevToolsEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export type SampleState = {
   text: string;
 };
