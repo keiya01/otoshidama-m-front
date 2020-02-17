8f1d801d283fd7e66fa2932d918fcfcb5d43081d
keiya01 <keiya.s.0210@gmail.com>
2020-02-17 14:57:51 +0900

    Add LotteryPage component

--- src/components/route/Router.tsx
+++ src/components/route/Router.tsx
@@ -1,12 +1,14 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import sample from './sample';
+import lotteryPage from './lotteryPage';
 
 const Router: React.FC = () => (
   <BrowserRouter>
     <Suspense fallback={<div>loading...</div>}>
       <Switch>
-        <Route path="/" component={sample} />
+        <Route exact path="/" component={lotteryPage} />
+        <Route path="/sample" component={sample} />
       </Switch>
     </Suspense>
   </BrowserRouter>
