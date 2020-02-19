8fc697eab05a936099efc99b8e25d8581074c0c3
keiya01 <keiya.s.0210@gmail.com>
2020-02-19 12:20:56 +0900

    Add SimpleSpinner component

---
+++ src/components/spinners/SimpleSpinner.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+interface SimpleSpinnerProps {
+  color: string;
+  borderWidth: number;
+  size: number;
+}
+
+const setSpinnerStyle = ({ color, borderWidth, size }: SimpleSpinnerProps) => `
+    border: ${borderWidth}px solid ${color};
+    border-top: ${borderWidth}px solid transparent;
+    width: ${size}px;
+    height: ${size}px;
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.span`
+  display: inline-block;
+  border-radius: 50%;
+  animation: ${spin} 700ms linear infinite;
+  ${setSpinnerStyle}
+`;
+
+export default function SimpleSpinner({
+  color, borderWidth, size,
+}: SimpleSpinnerProps): ReactElement {
+  return <Spinner role="img" aria-label="loading spinner" color={color} borderWidth={borderWidth} size={size} />;
+}
