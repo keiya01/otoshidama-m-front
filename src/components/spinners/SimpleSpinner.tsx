48a85fb2b81351f9a4b95bb8be0c329e90f92756
Kazushi Konosu <kazushi@konosu.me>
2020-02-20 20:57:10 +0900

    use arrow function for SimpleSpinner

--- src/components/spinners/SimpleSpinner.tsx
+++ src/components/spinners/SimpleSpinner.tsx
@@ -30,8 +30,17 @@ const Spinner = styled.span`
   ${setSpinnerStyle}
 `;
 
-export default function SimpleSpinner({
-  color, borderWidth, size,
-}: SimpleSpinnerProps): ReactElement {
-  return <Spinner aria-label="loading spinner" color={color} borderWidth={borderWidth} size={size} />;
-}
+const SimpleSpinner = ({
+  color,
+  borderWidth,
+  size,
+}: SimpleSpinnerProps): ReactElement => (
+  <Spinner
+    aria-label="loading spinner"
+    color={color}
+    borderWidth={borderWidth}
+    size={size}
+  />
+);
+
+export default SimpleSpinner;
