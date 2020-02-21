de3fe63e922b20b1018781097aeed37f5369c9bd
keiya01 <keiya.s.0210@gmail.com>
2020-02-21 11:05:00 +0900

    Update ResultPage

--- src/components/page/ResultPage.tsx
+++ src/components/page/ResultPage.tsx
@@ -1,34 +1,68 @@
-import React, { ReactElement } from 'react';
-import styled from 'styled-components';
+import React, { ReactElement, useCallback, useState } from 'react';
+import styled, { keyframes, css } from 'styled-components';
+import CheckBoard from '../backgrounds/CheckBoard';
+import OtoshidamaCard from '../cards/OtoshidamaCard';
+import ResultCard from '../cards/ResultCard';
+import LoadingLotteryModal from '../modals/LoadingLotteryModal';
 
-const Container = styled.div`
+const Container = styled(CheckBoard)`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100vh;
-  background-color: #fff;
+  position: relative;
 `;
 
-const TitleContainer = styled.div`
-  display: flex;
+const slide = keyframes`
+  0% {
+    transform: translateY(0) rotate(0deg) translateX(0);
+  }
+  20% {
+    transform: translateY(25%) rotate(7deg) translateX(-35px);
+  }
+  40% {
+    transform: translateY(20%) rotate(6deg) translateX(-30px);
+  }
+  100% {
+    transform: translateY(150%) rotate(35deg) translateX(-250px);
+  }
 `;
 
-const Title = styled.h1`
-  color: #E6BF43;
-  font-size: 5rem;
-  font-weight: 900;
-  text-shadow: 2px 2px 4px #aaa;
-  margin: 0 20px;
+const slideAnim = css`
+  animation: ${slide} 3s ease-out forwards;
 `;
 
-const ResultPage = (): ReactElement => (
-  <Container>
-    <TitleContainer>
-      <Title>当</Title>
-      <Title>選</Title>
-    </TitleContainer>
-  </Container>
+const setCardAnimation = ({ startAnimation }: { startAnimation: boolean }) => (
+  startAnimation && slideAnim
 );
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  z-index: 1;
+  ${setCardAnimation}
+`;
+
+const ResultPage = (): ReactElement => {
+  const [startAnimation, setStartAnimation] = useState(false);
+  const handleOnClick = useCallback(() => {
+    setStartAnimation(true);
+  }, [setStartAnimation]);
+
+  return (
+    <Container>
+      <CardWrapper startAnimation={startAnimation}>
+        <OtoshidamaCard />
+      </CardWrapper>
+      <ResultCard />
+      <LoadingLotteryModal fetching onClick={handleOnClick} />
+    </Container>
+  );
+};
 
 export default ResultPage;
